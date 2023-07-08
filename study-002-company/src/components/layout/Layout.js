cf7107688fdd3f3728437459225a9955a04b2dd7
sbpark88 <saebyulpark88@gmail.com>
2023-07-08 10:25:10 +0900

    Update: Apply background using images

--- study-002-company/src/components/layout/Layout.js
+++ study-002-company/src/components/layout/Layout.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-function Layout({ name, children }) {
+function Layout({ name, children, backgroundImage }) {
   return (
     <section className={`content ${name.toLowerCase()}`}>
-      <figure></figure>
+      {/*
+      React 는 SCSS 파일에 대한 접근을 하지 못하기 때문에 SCSS 에
+      배경 이미지를 지정하고 동적으로 가져와 컴파일 할 수 없기 때문에
+      props 로 전달하거나 styled component, tailwind css 등을 이용한다.
+       */}
+      <figure style={{ backgroundImage: backgroundImage }}></figure>
       <div className="inner">
         <h1>{name}</h1>
         {children}
