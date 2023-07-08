05fffece76c0803207f6d3af3d2289f0020a64f2
sbpark88 <saebyulpark88@gmail.com>
2023-07-08 11:12:56 +0900

    Update: Move route motion

--- study-002-company/src/components/layout/Layout.js
+++ study-002-company/src/components/layout/Layout.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 function Layout({ name, children, backgroundImage }) {
+  const section = useRef(null);
+
+  useEffect(function activation() {
+    const _section = section.current;
+    _section.classList.add("on");
+    return () => {
+      // useRef 로는 얘를 비동기적으로 처리가 안 된다.
+      // 나중에 forwardRef 를 사용해 해결.
+      _section.classList.remove("on");
+    };
+  }, []);
+
   return (
-    <section className={`content ${name.toLowerCase()}`}>
+    <section className={`content ${name.toLowerCase()}`} ref={section}>
       {/*
       React 는 SCSS 파일에 대한 접근을 하지 못하기 때문에 SCSS 에
       배경 이미지를 지정하고 동적으로 가져와 컴파일 할 수 없기 때문에
