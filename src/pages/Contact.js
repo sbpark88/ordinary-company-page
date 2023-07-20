6efe0b9b91fd8c724c95c6f60ba02359202d89e9
sbpark88 <saebyulpark88@gmail.com>
2023-07-20 13:59:58 +0900

    Update: Append external kakao library
    - Kakao Map API Library

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Layout from "../components/layout/Layout";
-import Constants from "../modules/data/Constants";
+import { kakaoMapApi } from "../apiKey";
+import { loadScript } from "../modules/utils/ImportExternalScript";
 
 function Contact(props) {
-  return <Layout name={"Contact"}>Contact</Layout>;
+  const kakaoMapRef = useRef(null);
+
+  const [loadKakaoMapScript, setLoadKakaoMapScript] = useState(null);
+
+  useEffect(() => {
+    let removeScript;
+    const loadKakaoMapApi = async () => {
+      try {
+        const result = await loadScript({ src: kakaoMapApiScriptSrc });
+        setLoadKakaoMapScript(result.load);
+        if (result.load) {
+          removeScript = result.removeScript;
+        }
+      } catch (e) {
+        console.error(`You cannot use kakao map, error: ${e}`);
+      }
+    };
+    loadKakaoMapApi();
+
+    return () => removeScript && removeScript();
+  }, []);
+
+  return <Layout name={"Contact"}></Layout>;
 }
 
 export default Contact;
+
+const kakaoMapApiBaseUrl = "//dapi.kakao.com/v2/maps/sdk.js?appkey=";
+const kakaoMapApiScriptSrc = kakaoMapApiBaseUrl + kakaoMapApi;
