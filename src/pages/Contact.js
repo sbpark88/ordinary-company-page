3278b3f3b9061a564070fde68f7bd5a99e5f1983
sbpark88 <saebyulpark88@gmail.com>
2023-07-20 20:28:00 +0900

    Update: Load kakao map
    # 카카오 공식 문서대로 하면 `LatLng`를
      찾을 수 없다고 나온다. script 는 정상적으로
      불러왔으나 해당 라이브러리를 가져오면 자동으로
      인스턴스 생성하며 다른 라이브러리를 추가로 스스로
      요청하는데 그 부분에서 문제가 생기는 것으로 보인다.
    
    # GET 요청을 할 때 `autoload=false`를 옵션으로
      주고, 인스턴스 생성을
      `kakao.maps.load(function(){})` 안에서
      하면 정상적으로 실행된다.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,17 +2,24 @@ import React, { useEffect, useRef, useState } from "react";
 import Layout from "../components/layout/Layout";
 import { kakaoMapApi } from "../apiKey";
 import { loadScript } from "../modules/utils/ImportExternalScript";
+import Constants from "../modules/data/Constants";
 
 function Contact(props) {
   const kakaoMapRef = useRef(null);
-
   const [loadKakaoMapScript, setLoadKakaoMapScript] = useState(null);
+  const [location, setLocation] = useState({
+    latitude: 33.450701, // 위도
+    longitude: 126.570667, // 경도
+  });
+  const { kakao } = window;
 
   useEffect(() => {
     let removeScript;
     const loadKakaoMapApi = async () => {
       try {
-        const result = await loadScript({ src: kakaoMapApiScriptSrc });
+        const result = await loadScript({
+          src: kakaoMapApiScriptSrc,
+        });
         setLoadKakaoMapScript(result.load);
         if (result.load) {
           removeScript = result.removeScript;
@@ -23,13 +30,34 @@ function Contact(props) {
     };
     loadKakaoMapApi();
 
-    return () => removeScript && removeScript();
+    // return () => removeScript && removeScript();
   }, []);
 
-  return <Layout name={"Contact"}></Layout>;
+  useEffect(() => {
+    if (loadKakaoMapScript) {
+      kakao.maps.load(function () {
+        const options = {
+          center: new kakao.maps.LatLng(location.latitude, location.longitude), // 지도의 중심좌표
+          level: 3, // 지도의 레벨(확대, 축소 정도)
+        };
+
+        const map = new kakao.maps.Map(kakaoMapRef.current, options);
+      });
+    }
+  }, [loadKakaoMapScript, location]);
+
+  return (
+    <Layout
+      name={"Contact"}
+      backgroundImageUrl={`${Constants.PUBLIC_URL}/img/Location.jpg`}
+    >
+      <div id="kakaoMap" ref={kakaoMapRef}></div>
+    </Layout>
+  );
 }
 
 export default Contact;
 
-const kakaoMapApiBaseUrl = "//dapi.kakao.com/v2/maps/sdk.js?appkey=";
+const kakaoMapApiBaseUrl =
+  "//dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=";
 const kakaoMapApiScriptSrc = kakaoMapApiBaseUrl + kakaoMapApi;
