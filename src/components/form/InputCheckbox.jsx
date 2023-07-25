1f89982035a8f6034bc36493b43f06c79acd4811
sbpark88 <saebyulpark88@gmail.com>
2023-07-25 12:26:06 +0900

    Add: Custom input checkbox component

---
+++ src/components/form/InputCheckbox.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+export function InputCheckbox({
+  label,
+  name,
+  data,
+  setData,
+  properties,
+  errorMessage,
+}) {
+  const handleChange = (event) => {
+    event.stopPropagation();
+    const { value, checked } = event.target;
+    setData(
+      checked ? [...new Set([...data, value])] : data.filter((v) => v !== value)
+    );
+  };
+
+  return (
+    <tr>
+      <th>{label}</th>
+      <td onChange={handleChange}>
+        {properties?.map((property) => (
+          <Checkbox
+            key={property}
+            name={name}
+            data={data}
+            property={property}
+          />
+        ))}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </td>
+    </tr>
+  );
+}
+
+function Checkbox({ name, data, property }) {
+  return (
+    <>
+      <label htmlFor={property}>{property}</label>
+      <input
+        type="checkbox"
+        name={name}
+        id={property}
+        defaultValue={property}
+        defaultChecked={data.includes(property)}
+      />
+    </>
+  );
+}
