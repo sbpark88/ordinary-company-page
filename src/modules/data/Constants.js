c0fbf879db004e245d0837f45c01cb1df1f3f1d0
sbpark88 <saebyulpark88@gmail.com>
2023-07-28 00:52:06 +0900

    Update: Append regExp constants

--- src/modules/data/Constants.js
+++ src/modules/data/Constants.js
@@ -1,9 +1,21 @@
 import { toast } from "react-toastify";
 
+/**
+ *
+ * PASSWORD_RULE_MIDDLE : 영문 + 숫자 + 특수문자
+ * PASSWORD_RULE_HARD : 영문 소문자 + 영문 대문자 + 숫자 + 특수문자
+ */
 const Constants = {
   PUBLIC_URL: process.env.PUBLIC_URL,
   TOAST_POSITION: {
     position: toast.POSITION.BOTTOM_CENTER,
+  },
+  REG_EXP: {
+    ALPHABETS: /[a-zA-Z]/,
+    LOWER_CASE: /[a-z]/,
+    UPPER_CASE: /[A-Z]/,
+    NUMBERS: /\d/,
+    SPECIAL_CHARACTERS: /[!@#$%^&*()_+]/,
   },
 };
 
