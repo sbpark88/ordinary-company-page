44e55fdc037d177674d3e6f80affe09f3e29c381
sbpark88 <saebyulpark88@gmail.com>
2023-07-27 21:47:09 +0900

    Test: Test new string utils
    - startWith
    - startWithAlphabet
    - startWithNumber
    - startWithSpecialCharacter
    - stringContains
    - stringContainsAlphabets
    - stringContainsLowerCases
    - stringContainsUpperCases
    - stringContainsNumbers
    - stringContainsSpecialCharacters
    
    Ref: #eef2af66

---
+++ src/__tests__/modules/utils/StringUtils.test.js
@@ -0,0 +1,186 @@
+import { expect, test } from "@jest/globals";
+import {
+  dropLongString,
+  startWithAlphabet,
+  startWithNumber,
+  startWithSpecialCharacter,
+  stringContainsAlphabets,
+  stringContainsLowerCases,
+  stringContainsUpperCases,
+  stringContainsNumbers,
+  stringContainsSpecialCharacters,
+  stringIsEmpty,
+  strLengthIsLessThanOrEqual,
+  strLengthIsGreaterThanOrEqual,
+} from "../../../modules/utils/StringUtils";
+
+// dropLongString 함수의 테스트
+test("Short strings are returned as origin.", () => {
+  const origin = "Hello";
+  const maxLength = 10;
+
+  const result = dropLongString(origin, maxLength);
+  expect(result).toBe(origin);
+  expect(result.length).toBeLessThanOrEqual(maxLength);
+});
+
+test("Strings longer than 20 characters are shortened.", () => {
+  const origin = "Hello, Mr. Jake.";
+  const maxLength = 10;
+  const expected = origin.slice(0, 10) + "...";
+
+  const result = dropLongString(origin, maxLength);
+  expect(result).not.toBe(origin);
+  expect(result.length).toBeLessThanOrEqual(maxLength + 3);
+  expect(result).toBe(expected);
+});
+
+// stringIsEmpty 함수의 테스트
+test("String is empty.", () => {
+  expect(stringIsEmpty(" ")).toBe(true);
+});
+
+test("String is non-empty.", () => {
+  expect(stringIsEmpty("Hello")).toBe(false);
+});
+
+// startWith 함수의 테스트
+test("String starts with alphabet", () => {
+  const testString = "Hello, World!";
+  expect(startWithAlphabet(testString)).toBe(true);
+});
+
+test("String does not start with alphabet - start with number", () => {
+  const testString = "9Hello, World!";
+  expect(startWithAlphabet(testString)).toBe(false);
+});
+
+test("String does not start with alphabet - start with special character", () => {
+  const testString = "$Hello, World!";
+  expect(startWithAlphabet(testString)).toBe(false);
+});
+test("String starts with number", () => {
+  const testString = "1234 Hello";
+  expect(startWithNumber(testString)).toBe(true);
+});
+
+test("String does not start with number - start with alphabet", () => {
+  const testString = "A1234 Hello";
+  expect(startWithNumber(testString)).toBe(false);
+});
+
+test("String does not start with number - start with special character", () => {
+  const testString = "$1234 Hello";
+  expect(startWithNumber(testString)).toBe(false);
+});
+
+test("String starts with special character", () => {
+  const testString = "!@#$ Hello";
+  expect(startWithSpecialCharacter(testString)).toBe(true);
+});
+
+test("String does not start with special character - start with alphabet", () => {
+  const testString = "Hello";
+  expect(startWithSpecialCharacter(testString)).toBe(false);
+});
+
+test("String does not start with special character - start with number", () => {
+  const testString = "2Hello";
+  expect(startWithSpecialCharacter(testString)).toBe(false);
+});
+
+test("String does not start with alphabet, number, or special character", () => {
+  const testString = " World!";
+  expect(startWithAlphabet(testString)).toBe(false);
+  expect(startWithNumber(testString)).toBe(false);
+  expect(startWithSpecialCharacter(testString)).toBe(false);
+});
+
+// stringIncludesAlphabets 함수의 테스트
+test("String contains as least one alphabet", () => {
+  const testString = "7*@4H24";
+  expect(stringContainsAlphabets(testString)).toBe(true);
+});
+
+test("String does not contain any alphabet", () => {
+  const testString = "7*@4824";
+  expect(stringContainsAlphabets(testString)).toBe(false);
+});
+
+// stringContainsLowerCases 함수의 테스트
+test("String contains at least one lowercase letter", () => {
+  const testString = "7*@424g";
+  expect(stringContainsLowerCases(testString)).toBe(true);
+});
+
+test("String does not contain any lowercase letter", () => {
+  const testString = "7*@424G";
+  expect(stringContainsLowerCases(testString)).toBe(false);
+});
+
+// stringContainsUpperCases 함수의 테스트
+test("String contains at least one uppercase letter", () => {
+  const testString = "7*@424G";
+  expect(stringContainsUpperCases(testString)).toBe(true);
+});
+
+test("String does not contain any uppercase letter", () => {
+  const testString = "7*@424g";
+  expect(stringContainsUpperCases(testString)).toBe(false);
+});
+
+// stringContainsNumbers 함수의 테스트
+test("String contains at least one number", () => {
+  const testString = "7*@Hsd";
+  expect(stringContainsNumbers(testString)).toBe(true);
+});
+
+test("String does not contain any number", () => {
+  const testString = "*@Hsd";
+  expect(stringContainsNumbers(testString)).toBe(false);
+});
+
+// stringContainsSpecialCharacters 함수의 테스트
+test("String contains at least one special character", () => {
+  const testString = "7Aa1^";
+  expect(stringContainsSpecialCharacters(testString)).toBe(true);
+});
+
+test("String does not contain any special character", () => {
+  const testString = "7Aa1";
+  expect(stringContainsSpecialCharacters(testString)).toBe(false);
+});
+
+// strLengthIsLessThanOrEqual 함수의 테스트
+test("String is shorter than or equal to 10 characters.", () => {
+  const length = 10;
+
+  const string8 = "abcde123";
+  const string10 = "abcde12345";
+  expect(strLengthIsLessThanOrEqual(string8, length)).toBe(true);
+  expect(strLengthIsLessThanOrEqual(string10, length)).toBe(true);
+});
+
+test("String is not shorter than or equal to 10 characters.", () => {
+  const length = 10;
+
+  const string11 = "abcde12345F";
+  expect(strLengthIsLessThanOrEqual(string11, length)).toBe(false);
+});
+
+// strLengthIsGreaterTHanOrEqual 함수의 테스트
+test("String is longer than or equal to 10 characters.", () => {
+  const length = 10;
+
+  const string10 = "abcde12345";
+  const string11 = "abcde12345F";
+  expect(strLengthIsGreaterThanOrEqual(string10, length)).toBe(true);
+  expect(strLengthIsGreaterThanOrEqual(string11, length)).toBe(true);
+});
+
+test("String is not longer than or equal to 10 characters.", () => {
+  const length = 10;
+
+  const string8 = "abcde123";
+  expect(strLengthIsGreaterThanOrEqual(string8, length)).toBe(false);
+});
