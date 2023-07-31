bda69ba60ad4511e4444b83e71d4e9cfc7e29d03
sbpark88 <saebyulpark88@gmail.com>
2023-07-31 19:36:55 +0900

    Update: Append 'community' data and msw api

--- src/mocks/handler.js
+++ src/mocks/handler.js
@@ -1,5 +1,5 @@
 import { rest } from "msw";
-import { members, news } from "./mockDb";
+import { members, news, communities } from "./mockDb";
 
 export const handlers = [
   rest.get("/member", (req, res, context) =>
@@ -8,4 +8,88 @@ export const handlers = [
   rest.get("/news", (req, res, context) =>
     res(context.status(200), context.json(news))
   ),
+  rest.get("/community", (req, res, context) =>
+    res(context.status(200), context.json(communities))
+  ),
+  rest.post("/community", (req, res, context) => {
+    const newCommunity = {
+      id: generateCommunityId(communities),
+      ...req.body,
+    };
+    communities.push(newCommunity);
+    return res(context.status(201));
+  }),
+  rest.put("/community/:id", (req, res, context) => {
+    const communityId = req.params.id;
+    const existingIndex = communities.findIndex(
+      (community) => community.id === communityId
+    );
+
+    if (existingIndex !== -1) {
+      communities[existingIndex] = req.body;
+      return res(context.status(200));
+    } else {
+      return res(
+        context.status(404),
+        context.json(resMessage("수정하려는 대상이 존재하지 않습니다."))
+      );
+    }
+  }),
+  rest.patch("/community/:id", (req, res, context) => {
+    const communityId = req.params.id;
+    const existingIndex = communities.findIndex(
+      (community) => community.id === communityId
+    );
+
+    if (existingIndex !== -1) {
+      communities[existingIndex] = {
+        ...communities[existingIndex],
+        ...req.body,
+      };
+      return res(context.status(200));
+    } else {
+      return res(
+        context.status(404),
+        context.json(resMessage("수정하려는 대상이 존재하지 않습니다."))
+      );
+    }
+  }),
+
+  rest.delete("/community/:id", (req, res, context) => {
+    const communityId = req.params.id;
+    const existingIndex = communities.findIndex(
+      (community) => community.id === communityId
+    );
+
+    if (existingIndex !== -1) {
+      communities.splice(existingIndex, 1);
+      return res(context.status(200));
+    } else {
+      return res(
+        context.status(404),
+        context.json(resMessage("수정하려는 대상이 존재하지 않습니다."))
+      );
+    }
+  }),
 ];
+
+const generateId =
+  (prefix) =>
+  (table = []) => {
+    const lastId = table
+      .flatMap((record) => record.id)
+      .toSorted((lhs, rhs) => lhs - rhs)
+      .pop();
+    if (lastId === undefined) {
+      return prefix + "00001";
+    } else {
+      return (
+        prefix + String(Number(lastId.replace(prefix, "")) + 1).padStart(5, "0")
+      );
+    }
+  };
+
+const generateNewsId = generateId("news");
+const generateCommunityId = generateId("community");
+
+const resMessage = (message) => ({ message: message });
--- src/mocks/mockDb.js
+++ src/mocks/mockDb.js
@@ -55,3 +55,36 @@ export const news = [
       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ad aliquam, aspernatur commodi delectus harum ipsum nulla, quasi quisquam, sint soluta voluptatem voluptates? Aliquid amet blanditiis doloribus possimus quaerat rerum.",
   },
 ];
+
+export const communities = [
+  {
+    id: "community00001",
+    title: "안녕하세요",
+    comment:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    id: "community00002",
+    title: "반갑습니다.",
+    comment:
+      "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    id: "community00003",
+    title: "날씨가 덥네요",
+    comment:
+      "Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi",
+  },
+  {
+    id: "community00004",
+    title: "잘 다녀갑니다.",
+    comment:
+      " No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.",
+  },
+  {
+    id: "community00005",
+    title: "좋은 하루 되세요",
+    comment:
+      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi",
+  },
+];
