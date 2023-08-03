debe8fb03b28eb2d534a9c69331590b314e798d7
sbpark88 <saebyulpark88@gmail.com>
2023-08-03 22:02:29 +0900

    Fix: Change the wrong url path

--- src/modules/api/Members.js
+++ src/modules/api/Members.js
@@ -2,5 +2,5 @@ import axios from "axios";
 import $K from "../data/Constants";
 
 export const getMembers = async () => {
-  return await axios.get(`${$K.PUBLIC_URL}/members`);
+  return await axios.get(`${$K.PUBLIC_URL}/member`);
 };
