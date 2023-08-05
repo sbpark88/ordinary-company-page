8f37126d1a01a3b3270dccb6d6a452b7ee955612
sbpark88 <saebyulpark88@gmail.com>
2023-08-05 23:40:16 +0900

    Feat: Fetching data and draw images on gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,16 +1,82 @@
-import React from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 import Layout from "../components/layout/Layout";
 import $K from "../modules/data/Constants";
+import { getFlickrImagesOfInterest } from "../modules/api/Gallery";
+import Modal from "../components/layout/Modal";
 
-function Gallery(props) {
+function Gallery() {
+  const [galleries, setGalleries] = useState();
+  const [selectedImageUrl, setSelectedImageUrl] = useState();
+  const modal = useRef(null);
+
+  const getInterest = async () => {
+    const response = await getFlickrImagesOfInterest();
+    setGalleries(response.data.photos.photo);
+  };
+
+  useEffect(getInterest, []);
+
   return (
-    <Layout
-      name={"Gallery"}
-      backgroundImageUrl={`${$K.PUBLIC_URL}/img/Gallery.jpg`}
-    >
-      Gallery
-    </Layout>
+    <>
+      <Layout
+        name={"Gallery"}
+        backgroundImageUrl={`${$K.PUBLIC_URL}/img/Gallery.jpg`}
+      >
+        <div className="frame">
+          {galleries?.map((gallery) => (
+            <GalleryCard
+              key={gallery.id}
+              {...gallery}
+              modal={modal}
+              setSelectedImageUrl={setSelectedImageUrl}
+            />
+          ))}
+        </div>
+      </Layout>
+      <Modal ref={modal}>
+        <img src={selectedImageUrl} alt="large picture" />
+      </Modal>
+    </>
   );
 }
 
 export default Gallery;
+
+const defaultBuddyIcon = "https://www.flickr.com/images/buddyicon.gif";
+
+const GalleryCard = memo((photo, modal, setSelectedImageUrl) => {
+  const { id, server, secret, title, farm, owner } = photo;
+
+  const middlePictureImageUrl = `https://live.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
+  const bigPictureImageUrl = `https://live.staticflickr.com/${server}/${id}_${secret}_b.jpg`;
+  const profileImageUrl = `https://farm${farm}.staticflickr.com/${server}/buddyicons/${owner}.jpg`;
+
+  const openModal = () => {
+    console.log("open modal");
+    modal.current.openModal();
+  };
+
+  const searchByUser = () => {
+    console.log("search by user");
+    setSelectedImageUrl(bigPictureImageUrl);
+  };
+
+  return (
+    <article>
+      <div className="inner">
+        <div className="pic">
+          <img src={middlePictureImageUrl} alt={title} onClick={openModal} />
+        </div>
+        <h2>{title}</h2>
+        <div className="profile">
+          <img
+            src={profileImageUrl}
+            alt={owner}
+            onError={(e) => e.target.setAttribute("src", defaultBuddyIcon)}
+          />
+          <span onClick={searchByUser}>{owner}</span>
+        </div>
+      </div>
+    </article>
+  );
+});
